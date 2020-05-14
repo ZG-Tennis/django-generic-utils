7f8399bb335918a952ebd7df4570144cad1a1a9a
Reiner Marquez <rmaceissoft@gmail.com>
2020-05-13 19:31:42 -0500

    get rid of django.conf.urls.patterns

--- generics/urls.py
+++ generics/urls.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url #include,
+from django.conf.urls import url
 
-# import logging
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
+from . import views
 
-
-urlpatterns = patterns(
-    '',
-    url(r'^task_api$', 'generics.views.task_api', name="task_api"),
-    url(r'^celery_test$', 'generics.views.celery_test', name="celery_test"),
-    url(r'^messages_api$', 'generics.views.messages_api', name="messages_api"),
-    )
-
+urlpatterns = [
+    url(r'^task_api$', views.task_api, name="task_api"),
+    url(r'^celery_test$', views.celery_test, name="celery_test"),
+    url(r'^messages_api$', views.messages_api, name="messages_api"),
+]
